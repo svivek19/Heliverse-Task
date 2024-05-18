0c137aa0eb422fa2fc4e2a309be44b175fddb2c5
svivek19 <svivek030503@gmail.com>
2024-05-18 12:40:00 +0530

    navbar finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Logo from "../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#262626] p-4">
+      <div className="flex justify-between items-center">
+        <img src={Logo} alt="brand-logo" width={150} />
+        <button className="bg-[#82b440] text-white px-4 py-1 rounded">
+          Buy now
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
