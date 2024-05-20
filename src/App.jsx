cc90e068513dcd9c15462b611f6ebedad8d49e8c
Vivekananthan S <129610457+svivek19@users.noreply.github.com>
2024-05-20 10:55:15 +0530

    final App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -8,21 +8,33 @@ import SectionAndPage from "./components/SectionAndPage";
 import SupportedPage from "./components/SupportedPage";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
-import CustomCursor from "./components/animation/CustomCursor";
+import { particles } from "./components/animation/CustomCursor";
+
+const isMediumScreenOrLarger = () => {
+  return window.matchMedia("(min-width: 768px)").matches;
+};
 
 const App = () => {
+  useEffect(() => {
+    if (isMediumScreenOrLarger()) {
+      particles();
+    }
+  }, []);
+
   return (
     <>
-      <CustomCursor />
-      <Navbar />
-      <Header />
-      <Hero />
-      <Ratings />
-      <PurchaseSection />
-      <SectionAndPage />
-      <SupportedPage />
-      <Features />
-      <Footer />
+      <canvas></canvas>
+      <main>
+        <Navbar />
+        <Header />
+        <Hero />
+        <Ratings />
+        <PurchaseSection />
+        <SectionAndPage />
+        <SupportedPage />
+        <Features />
+        <Footer />
+      </main>
     </>
   );
 };
